b2f206e6725f455cc1a276af3053b377a61a0280
David Gaytan <edgaytanc@gmail.com>
2024-10-14 11:14:00 -0600

    Se agrego Seguridad y Autenticacion JWT

--- Dicaprios/settings.py
+++ Dicaprios/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -86,7 +87,7 @@ DATABASES = {
         'NAME': 'dicaprios_db',
         'USER': 'root',
         'PASSWORD': 'edgc2001--',
-        'HOST': '192.168.31.73',
+        'HOST': '192.168.31.74',
         'PORT': '3306',
     }
 }
@@ -138,14 +139,23 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",  # URL donde corre tu frontend (React, por ejemplo)
 ]
 
-# REST_FRAMEWORK = {
-#     'DEFAULT_AUTHENTICATION_CLASSES': (
-#         'rest_framework_simplejwt.authentication.JWTAuthentication',
-#     ),
-#     'DEFAULT_PERMISSION_CLASSES': (
-#         'rest_framework.permissions.IsAuthenticated',
-#     ),
-# }
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': True,
+    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': SECRET_KEY,
+}
 
 # O permitir todas las solicitudes (solo para desarrollo)
 CORS_ALLOW_ALL_ORIGINS = True
--- Dicaprios/urls.py
+++ Dicaprios/urls.py
@@ -23,6 +23,6 @@ router.register(r'detalles-factura', DetalleFacturaViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
-    # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
