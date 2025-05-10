330534b11af50d8c61bf2591ca5d12e6ee69fc8c
David Gaytan <edgaytanc@gmail.com>
2025-05-10 01:21:25 -0600

    Preparacion par la implementacion del nuevo modulo

--- Dicaprios/settings.py
+++ Dicaprios/settings.py
@@ -12,10 +12,14 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+from dotenv import load_dotenv
+import os
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -45,6 +49,7 @@ INSTALLED_APPS = [
     'clientes',
     'productos',
     'ventas',
+    'ia',
 ]
 
 MIDDLEWARE = [
